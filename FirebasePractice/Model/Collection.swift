400c13b438337362d9837ef253fb22cdb55c448c
Yi-Chin Hsu <ychim26@gmail.com>
2021-09-23 14:44:06 +0800

    Feat: Login check feature built

---
+++ FirebasePractice/Model/Collection.swift
@@ -0,0 +1,45 @@
+//
+//  Collection.swift
+//  FirebasePractice
+//
+//  Created by Yi-Chin Hsu on 2021/9/23.
+//
+
+import Foundation
+
+enum Collection: CaseIterable{
+    case articles
+    case users
+    case friends
+    
+    var title: String {
+        switch self {
+        case .articles:
+            return "Articles"
+        case .users:
+            return "Users"
+        case .friends:
+            return "Friends"
+        }
+    }
+    
+}
+
+enum Users: CaseIterable {
+    
+    case id
+    case email
+    case name
+    
+    var field: String {
+        switch self {
+        case .id:
+            return "id"
+        case .email:
+            return "email"
+        case .name:
+            return "name"
+        }
+    }
+    
+}
---
+++ FirebasePractice/View Controller/LoginViewController.swift
@@ -0,0 +1,92 @@
+//
+//  LoginViewController.swift
+//  FirebasePractice
+//
+//  Created by Yi-Chin Hsu on 2021/9/23.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    var userHasAccount = true
+    var loginEmail = ""
+    
+    let db = Firestore.firestore()
+
+    
+    @IBOutlet weak var emailTextField: UITextField! {
+        didSet {
+            emailTextField.delegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func pressLogin(_ sender: Any) {
+        
+        findUserEmail { hasAccount in
+            if hasAccount {
+                self.performSegue(withIdentifier: "loginSuccess", sender: nil)
+            } else {
+                self.performSegue(withIdentifier: "toSignUp", sender: nil)
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let homeVC = segue.destination as? TabBarController {
+            homeVC.modalPresentationStyle = .fullScreen
+        }
+        
+        if let signUpVC = segue.destination as? SignUpViewController {
+            signUpVC.modalPresentationStyle = .fullScreen
+        }
+    }
+
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let text = textField.text,
+              !text.isEmpty else {
+            print("Please enter a valid input")
+            return
+        }
+        
+        loginEmail = text
+    }
+    
+}
+
+extension LoginViewController {
+    
+    func findUserEmail(completion: @escaping (Bool) -> Void) {
+        
+        userHasAccount = false
+        
+        db.collection(Collection.users.title).whereField(Users.email.field, isEqualTo: loginEmail).getDocuments {
+            (querySnapshot, error) in
+            if let querySnapshot = querySnapshot {
+                for document in querySnapshot.documents {
+                    print(document.data())
+                    self.userHasAccount = true
+                    completion(self.userHasAccount)
+                    break
+                }
+                
+            }
+            self.userHasAccount = false
+            completion(self.userHasAccount)
+        }
+        
+        
+    }
+    
+    
+}
